2fcbe348e3a45a9d0609b4ce14bee7d94b029f9b
Ned Letcher <nletcher@gmail.com>
2019-03-28 15:17:39 +1100

    Add support for guessing mimetypes for static files

--- dash_test/viz/urls.py
+++ dash_test/viz/urls.py
@@ -4,6 +4,8 @@ from . import views
 from . import dashapp # this loads the Dash app
 
 urlpatterns = [
-    re_path('^_dash-', views.dash_ajax),
+    re_path('^_dash-', views.dash_json),
+    re_path('^assets/', views.dash_guess_mimetype),
     re_path('^', views.dash_index),
+    
 ]
--- dash_test/viz/views.py
+++ dash_test/viz/views.py
@@ -1,3 +1,5 @@
+import mimetypes
+
 from django.http.response import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
@@ -17,20 +19,30 @@ def dispatcher(request):
     }
     with server.test_request_context(request.path, **params):
         server.preprocess_request()
+
         try:
             response = server.full_dispatch_request()
         except Exception as e:
             response = server.make_response(server.handle_exception(e))
+
+        response.direct_passthrough = False
         return response.get_data()
 
 
-def dash_index(request, **kwargs):
-    ''' '''
-    return HttpResponse(dispatcher(request))
-
-
 @csrf_exempt
-def dash_ajax(request):
-    ''' '''
+def dash_json(request, **kwargs):
+    """Handle Dash JSON API requests"""
+    print(request.get_full_path())
     return HttpResponse(dispatcher(request), content_type='application/json')
 
+
+def dash_index(request, **kwargs):
+    """Handle Dash CSS requests"""
+    return HttpResponse(dispatcher(request), content_type='text/html')
+
+
+def dash_guess_mimetype(request, **kwargs):
+    """Handle Dash requests and guess the mimetype. Needed for static files."""
+    url = request.get_full_path().split('?')[0]
+    content_type, _encoding = mimetypes.guess_type(url)
+    return HttpResponse(dispatcher(request), content_type=content_type)
